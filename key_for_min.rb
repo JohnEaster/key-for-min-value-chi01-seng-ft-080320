2bfd65b2df2522651d5b93a654919429d70f938a
John Easter <john@owltestprep.com>
2020-07-18 15:06:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(name_hash = { :blake => 500, :asheley => 3,  :adam => 1 })
+  lowest_number = 1000
+  name_hash.each do |name, number|
+    if number < lowest_number
+      name
+    end
+  end
+name
 end
+
+
