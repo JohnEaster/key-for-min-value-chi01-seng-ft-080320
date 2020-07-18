83f186c47b86e1f0ccfade7cff919533e9834d40
John Easter <john@owltestprep.com>
2020-07-18 15:12:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash = { :blake => 500, :asheley => 3,  :adam => 1 })
-  lowest_number = 1000
-  name_hash.each do |name, number|
-    if number < lowest_number
-      name
+  lowest_key = nil
+  lowest_value = nil
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
     end
   end
-name
+  lowest_key
 end
-
-
